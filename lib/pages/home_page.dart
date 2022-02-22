e0534abe3be1c2ee2452c4f9fec0dd5041416304
Shehryarrr <shehryarshk@gmail.com>
2022-02-22 17:03:16 +0500

    day12 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,32 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
 import 'package:flutter_catlog/models/catalog.dart';
 import 'package:flutter_catlog/widgets/drawer.dart';
-import 'package:flutter_catlog/widgets/item_widget.dart';   
+import 'package:flutter_catlog/widgets/item_widget.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   final int days = 30;
+
   final String name = "Dart";
 
   @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    final catalogJson =
+        await rootBundle.loadString("assets/files/catalog.json");
+    final decodeData = jsonDecode(catalogJson);
+    var productsData = decodeData["products"];
+    print(productsData);
+  }
+
+  @override
   Widget build(BuildContext context) {
-     final dummyList = List.generate(20, (index) => CatalogModel.items[0]);
+    final dummyList = List.generate(20, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
-        
-        title:const Text("Catalog App"),
+        title: const Text("Catalog App"),
       ),
-      body:  Padding(
+      body: Padding(
         padding: const EdgeInsets.all(18.0),
         child: ListView.builder(
-          itemCount:dummyList.length,
-          itemBuilder: (context , index){
-           return ItemWidget( item:  dummyList[index],); 
+          itemCount: dummyList.length,
+          itemBuilder: (context, index) {
+            return ItemWidget(
+              item: dummyList[index],
+            );
           },
         ),
       ),
-      
       drawer: const MyDrawer(),
     );
   }
