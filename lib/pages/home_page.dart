471aeb00c7baf07cba923d908cb91fb0c430bd4e
Shehryarrr <shehryarshk@gmail.com>
2022-03-01 15:06:45 +0500

    Day16 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,9 +1,11 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'dart:convert';
+import 'package:velocity_x/velocity_x.dart';
+
 import 'package:flutter_catlog/models/catalog.dart';
 import 'package:flutter_catlog/widgets/themes.dart';
-import 'package:velocity_x/velocity_x.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -94,16 +96,56 @@ class CatalogList extends StatelessWidget {
 class CatalogItem extends StatelessWidget {
   final Item catalog;
 
-  const CatalogItem({Key? key, required this.catalog}) :
-  super(key: key);
-
+  const CatalogItem({Key? key, required this.catalog}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return VxBox(
-      child: Row(children: [Image.network(catalog.image,)
-      ],
+      child: Row(
+        children: [
+          CatalogImage(image: catalog.image,),
+          Expanded(child: Column(
+            crossAxisAlignment:CrossAxisAlignment.start,
+            mainAxisAlignment:MainAxisAlignment.center,
+            children: [
+              catalog.name.text.lg.color(MyTheme.darkBluish).bold.make(),
+              catalog.desc.text.textStyle(context.captionStyle!).make(),
+              10.heightBox,
+              ButtonBar(
+                alignment: MainAxisAlignment.spaceBetween,
+                buttonPadding: EdgeInsets.zero,
+
+                children: [
+                "\$${catalog.price}".text.bold.lg.make(),
+                ElevatedButton(onPressed: () {},
+                style: ButtonStyle(backgroundColor: MaterialStateProperty.all(MyTheme.darkBluish),
+                shape: MaterialStateProperty.all(const StadiumBorder(),
+                ),
+                ),
+                 child: "Buy".text.make(),
+                ),
+              ],
+            ).pOnly(right: 8.0)
+            ],
+          ),
+          ),
+        ],
       ),
-    ).white.square(100).make();
+    ).white.rounded.square(100).make().py16();
+  }
+}
+
+class CatalogImage extends StatelessWidget {
+  final String image;
+  const CatalogImage({
+    Key? key,
+    required this.image,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.network(
+      image,
+    ).box.rounded.p8.color(MyTheme.creamColor).make().p12().w40(context);
   }
 }
